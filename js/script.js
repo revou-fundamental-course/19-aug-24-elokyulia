128ac5a4ec677ac6994dbc1c538c7997a2d3fa3b
Yulia Danita <elokyulia69@gmail.com>
2024-08-29 12:01:34 +0700

    test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-//ini javascript
+//ini javascrip
 
-function <replacename{[]{
-    
-}
 
+<html lang="en">
+
+<head>
+   
+</head>
+
+<body>
+   <h1>Hi <span>Yulia</span>Welcom to my Webside </h1> 
+   <script>scr =script.js</script>
+</body>
+
+</html>
+
+        
+       
